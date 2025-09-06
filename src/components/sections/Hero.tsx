4cac93e5572ed096e547fea143568509f8130758
Santiago Lobo Ulloa <lobo09.slu@gmail.com>
2025-09-05 21:48:59 -0600

    New components and hero section V1

---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,48 @@
+import StarBorder from "@/blocks/StarBorder/StarBorder";
+import RotatingText from "../../blocks/TextAnimations/RotatingText/RotatingText";
+
+export default function Hero() {
+  return (
+    <div className="hero w-screen h-screen bg-radial from-[#1b4b4c] to-[#0d2a2b] flex items-center justify-center">
+      <div className="hero-content w-full h-full flex flex-col items-center justify-center gap-12">
+        <div className="flex flex-col items-center justify-start gap-2 w-full text-center px-4">
+          <h1 className="xl:text-5xl sm:text-4xl md:text-5xl text-3xl font-bold text-white text-center">
+            Hello, I am
+          </h1>
+          {/* Prevent line wrapping with whitespace-nowrap and responsive text sizing */}
+          <div className="w-full flex items-center justify-center">
+            <RotatingText
+              mainClassName="text-3xl sm:text-4xl md:text-5xl xl:text-5xl font-bold text-white text-center justify-center items-center whitespace-nowrap"
+              texts={[
+                "Santiago Lobo",
+                "a Software Developer",
+                "a leader",
+                "an innovator",
+                "an entrepreneur",
+              ]}
+              rotationInterval={3000}
+              splitBy="characters"
+              animatePresenceInitial={true}
+              staggerDuration={0.02}
+              exit={{ opacity: 0 }}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-center gap-3">
+          <StarBorder
+            as="button"
+            color="#00B557"
+            thickness={2}
+            className=" cursor-pointer font-medium"
+            innerClassName="btn btn-md bg-emerald-800 hover:bg-emerald-900 text-white hover:bg-emerald-900 border-0 font-medium"
+          >
+            View Projects
+          </StarBorder>
+          <button className=" btn btn-md border-2 border-white/30 text-white bg-transparent hover:bg-white/10 font-medium">
+            Download Resume
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
